c1f99e82de725d6e905f639a6241b6171280cac3
Iliay Belous <i.l.i.a.y.b.e.l.o.u.s@gmail.com>
2022-08-12 11:56:05 +0300

    init Zulip app

---
+++ clients/zulip/zulip.go
@@ -0,0 +1,38 @@
+package zulip
+
+import (
+	"github.com/dghubble/sling"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	client   *http.Client
+	sling    *sling.Sling
+	Email    string
+	APIKey   string
+	common   service
+	Messages *MessagesService
+	Events   *EventsService
+}
+
+type service struct {
+	client *Client
+}
+
+func New(httpClient *http.Client, baseURL string) *Client {
+	if httpClient == nil {
+		httpClient = &http.Client{
+			Timeout: time.Second * 10,
+		}
+	}
+
+	s := sling.New().Client(httpClient).Base(baseURL)
+	c := &Client{sling: s}
+
+	c.common.client = c
+	c.Messages = (*MessagesService)(&c.common)
+	c.Events = (*EventsService)(&c.common)
+
+	return c
+}
---
+++ events/zulip/messages.go
@@ -0,0 +1,33 @@
+package zulip
+
+type MessagesService service
+
+type MessageResponse struct {
+	Msg    string `json:"msg"`
+	Result string `json:"result"`
+	ID     int64  `json:"id"`
+}
+
+type MessageRequest struct {
+	Type    string `json:"type"`
+	To      string `json:"to"`
+	Subject string `json:"subject,omitempty"`
+	Content string `json:"content"`
+}
+
+func (s *MessagesService) Send(type_ string, to string, subject string, content string) (_ int64, err error) {
+	body := &MessageRequest{
+		Type:    type_,
+		To:      to,
+		Subject: subject,
+		Content: content,
+	}
+	resp := new(MessageResponse)
+
+	_, err = s.client.sling.New().Post("messages").BodyJSON(body).Receive(resp, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	return resp.ID, err
+}
